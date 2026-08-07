975652fd494fd9c3d9d52e7c123712077df6b71a
Shadrack Sithole <shadrackdrack@gmail.com>
2026-08-07 12:03:44 +0200

    test(api): add request specs for transactions endpoint

--- backend/app/controllers/api/v1/transactions_controller.rb
+++ backend/app/controllers/api/v1/transactions_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::TransactionsController < ApplicationController
   def index
-    Rails.logger.info "******** TRANSACTIONS CONTROLLER WAS HIT ********"
     transactions = Transaction.order(processed_at: :desc)
+
+    if params[:status].present?
+      transactions = transactions.where(status: params[:status])
+    end
 
     render json: TransactionSerializer.serialize_collection(transactions)
   end
